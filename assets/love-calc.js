f6c870c657efc66eecccee4286ebc3e1525d51d3
Monique Levey <monique.w.91@outlook.com>
2022-03-24 21:43:07 +0800

    commented skeleton structure in love-cal js to begin to code fetch functions

---
+++ assets/love-calc.js
@@ -0,0 +1,16 @@
+// when the page renders
+// then I want to see a love calculator button or a help me now button
+// when I click on the love calculator button
+// then I want to see two inputs for love calculator names
+// when I type in the two names into the input
+// then I want to see a percentage render (in the heart) of our love likelihood
+// and I want the input names to disappear
+
+// if the percentage renders and it is below 50 (low)
+// then I want to see an escape now with a good excuse (excuse generator) button show
+
+// else if the percentage renders and it is higher than 50 (high)
+// then I want to see a fun date activities (activity generator) button show
+
+// else when a percentage renders (ALL) 
+// then 
