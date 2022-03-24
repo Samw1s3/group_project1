198b185fbe046d5f9ca9766ab3f644c810da5487
Monique Levey <monique.w.91@outlook.com>
2022-03-24 22:06:29 +0800

    updated the comments in love-calc js

--- assets/love-calc.js
+++ assets/love-calc.js
@@ -1,16 +1,32 @@
 // when the page renders
 // then I want to see a love calculator button or a help me now button
+
 // when I click on the love calculator button
 // then I want to see two inputs for love calculator names
+
+
 // when I type in the two names into the input
+    // save names & stringify in local storage
+    // send as names object to api? 
+    // fetch data from api for percentage
 // then I want to see a percentage render (in the heart) of our love likelihood
+    // render at an h1 element
+    // render a percentage sign
 // and I want the input names to disappear
+// render a try again button at top of heart 
+
+
 
 // if the percentage renders and it is below 50 (low)
+// then I want a gif of a broken heart to display briefly (6seconds)
+    // z-index higher
 // then I want to see an escape now with a good excuse (excuse generator) button show
 
 // else if the percentage renders and it is higher than 50 (high)
+// then I want a gif of a beating heart to display briefly (6seconds) 
+    // z-index higher
 // then I want to see a fun date activities (activity generator) button show
 
 // else when a percentage renders (ALL) 
-// then 
+// then a weather card (mini dashboard or icon) shows
+// and a joke generator button shows 
