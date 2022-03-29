5130a40f68a20b136fd5a199eedfb0032c06f0e3
Sam Knopper <samanthaknopper@hotmail.com>
2022-03-29 19:23:17 +0800

    activity generator done

--- assets/activity-generator.js
+++ assets/activity-generator.js
@@ -1,13 +1,52 @@
 let url = "http://www.boredapi.com/api/activity?participants=2"
 
-fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
+let recURL = "http://www.boredapi.com/api/activity?type=recreational"
+
+const activityText = document.getElementById('activity-text');
+
+const actButton = document.getElementById('activity-button');
+
+const recText = document.getElementById("reacreation-text");
+
+const recButton = document.getElementById("recreation-button")
+  // user hits button
+actButton.addEventListener('click',function(event){
+    event.preventDefault();
+
+    fetch(url)
+         .then(function (response) {
+         return response.json();
+        })
     .then(function (data) {
       console.log(data);
-    })
+    
+    const activity = data.activity
 
-    // user hits button
-    // random activity is displayed below the button
+    activityText.textContent = activity
+
+    console.log(data.activity);
+
+    })
+})
+
+recButton.addEventListener('click',function(event){
+    event.preventDefault();
+
+    fetch(recURL)
+         .then(function (response) {
+         return response.json();
+        })
+    .then(function (data) {
+      console.log(data);
+    
+    const activity = data.activity
+
+    recText.textContent = activity
+
+    console.log(data.activity);
+
+    })
+}
+
+)    // random activity is displayed below the button
     
